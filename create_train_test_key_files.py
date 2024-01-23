b1db23e0cd27bff3c715f3e42790d243319e603d
rt-desai <abhyuday.desai@readytensor.com>
2024-01-22 21:47:43 -0800

    create config files; create schema files; create train, test, and test-key files

---
+++ create_train_test_key_files.py
@@ -0,0 +1,168 @@
+import os
+import pandas as pd
+from pandas import DataFrame
+from typing import List
+import sys
+
+import utils
+import paths
+
+
+
+def save_train_data(
+        train_df: DataFrame, dataset_name: str, processed_datasets_path: str) -> None:
+    """
+    Saves the train data to a CSV file.
+
+    Args:
+        train_df (DataFrame): The train dataset.
+        dataset_name (str): The name of the dataset.
+        processed_datasets_path (str): The path where the processed datasets are stored.
+    """
+    train_df.to_csv(os.path.join(
+        processed_datasets_path, dataset_name, f"{dataset_name}_train.csv"), index=False)
+
+def save_test_no_target_data(
+        test_df: DataFrame,
+        target_name: str,
+        past_covariates: List[str],
+        dataset_name: str,
+        processed_datasets_path: str) -> None:
+    """
+    Saves the test data without the target column to a CSV file.
+
+    Args:
+        test_df (DataFrame): The test dataset.
+        target_name (str): The name of the target column.
+        past_covariates (List[str]): The names of the past covariates.
+        dataset_name (str): The name of the dataset.
+        processed_datasets_path (str): The path where the processed datasets are stored.
+    """
+    test_no_target_df_no_past_cov = test_df.drop(columns=past_covariates + [target_name], axis=1)
+    test_no_target_df_no_past_cov.to_csv(
+        os.path.join(processed_datasets_path, dataset_name, f"{dataset_name}_test.csv"), index=False)
+
+
+def get_past_covariates(schema: dict) -> list:
+    """
+    Extracts the names of all past covariates from a schema dictionary.
+
+    Args:
+        schema (dict): A schema represented as a dictionary.
+
+    Returns:
+        list: A list containing the names of all past covariates. Returns
+        an empty list if there are no past covariates in the schema.
+
+    Example:
+        schema = { ... }  # your schema dictionary
+        past_covariates = get_past_covariates(schema)
+        print(past_covariates)
+    """
+    # Check if 'pastCovariates' exists in the schema and is a list
+    if 'pastCovariates' in schema and isinstance(schema['pastCovariates'], list):
+        # Extract and return the names of the past covariates
+        return [covariate['name'] for covariate in schema['pastCovariates']]
+
+    # Return an empty list if there are no past covariates
+    return []
+
+
+def save_test_key_data(
+        test_df: DataFrame,
+        id_name: str,
+        time_name: str,
+        target_name: str,
+        dataset_name: str,
+        processed_datasets_path: str) -> None:
+    """
+    Saves the test key data to a CSV file.
+
+    Args:
+        test_df (DataFrame): The test dataset.
+        id_name (str): The name of the ID column.
+        time_name (str): The name of the time column.
+        target_name (str): The name of the target column.
+        dataset_name (str): The name of the dataset.
+        processed_datasets_path (str): The path where the processed datasets are stored.
+    """
+    test_key_df = test_df[[id_name, time_name, target_name]]
+    test_key_df.to_csv(
+        os.path.join(processed_datasets_path, dataset_name, f"{dataset_name}_test_key.csv"), index=False)
+
+
+def create_train_test_testkey_files(dataset_cfg_path:str, processed_datasets_path:str) -> None:
+    """
+    Creates train, test, and test key files for each dataset marked for use in the metadata.
+    """
+
+    # Load the metadata
+    dataset_metadata = utils.load_metadata(dataset_cfg_path)
+
+    # Iterate through each dataset marked for use in the metadata
+    for _, dataset_row in dataset_metadata[dataset_metadata['use_dataset'] == 1].iterrows():
+
+        if dataset_row["use_dataset"] == 0:
+            continue
+
+        dataset_name = dataset_row["name"]
+        print("Creating train/test files for dataset:", dataset_name)
+
+        # Read dataset
+        dataset = utils.load_dataset(dataset_name, processed_datasets_path)
+
+        # Read schema
+        schema = utils.load_schema(dataset_name, processed_datasets_path)
+
+        if schema["timeField"]["dataType"] != "INT":
+            dataset[schema["timeField"]["name"]] = pd.to_datetime(dataset[schema["timeField"]["name"]])
+
+        dataset_train_dfs = []
+        dataset_test_dfs = []
+
+        dataset_series_list = dataset[schema['idField']['name']].unique().tolist()
+        forecast_length = schema['forecastLength']
+
+        for series in dataset_series_list:
+            series_df = dataset[dataset[schema['idField']['name']] == series]
+            series_train_df = series_df.iloc[:-forecast_length]
+            series_test_df = series_df.iloc[-forecast_length:]
+            dataset_train_dfs.append(series_train_df)
+            dataset_test_dfs.append(series_test_df)
+            # print(series, series_df.shape, series_train_df.shape, series_test_df.shape)
+        
+        train_df = pd.concat(dataset_train_dfs)
+        test_df = pd.concat(dataset_test_dfs)
+
+        # Save train/test data
+        save_train_data(train_df, dataset_name, processed_datasets_path)
+
+        # Save test data without target
+        past_covariates = get_past_covariates(schema)
+        save_test_no_target_data(
+            test_df,
+            schema['forecastTarget']['name'],
+            past_covariates, # these will be dropped from the test data
+            dataset_name,
+            processed_datasets_path)
+
+        # Save test key data
+        save_test_key_data(
+            test_df,
+            schema['idField']['name'],
+            schema['timeField']['name'],
+            schema['forecastTarget']['name'],
+            dataset_name,
+            processed_datasets_path
+        )
+
+
+def run_train_test_testkey_files_gen():
+    """Creates train, test, and test key files for each dataset marked for use in the metadata."""
+    create_train_test_testkey_files(
+        dataset_cfg_path=paths.dataset_cfg_path,
+        processed_datasets_path=paths.processed_datasets_path
+    )
+
+if __name__ == "__main__":
+    run_train_test_testkey_files_gen()
---
+++ generate_schemas.py
@@ -0,0 +1,270 @@
+import os
+import pandas as pd
+import json
+from typing import Dict, List, Union
+from utils import load_metadata, load_features_config, strip_quotes, JSONEncoder
+
+import paths
+
+
+def filter_features_for_dataset(
+        dataset_name: str,
+        field_type: str,
+        features_config: pd.DataFrame) -> pd.DataFrame:
+    """
+    Filters the features configuration for the given dataset and field type.
+
+    Args:
+    dataset_name (str): The name of the dataset.
+    field_type (str): The type of field to filter for.
+    features_config (pd.DataFrame): The features configuration data.
+
+    Returns:
+    pd.DataFrame: The filtered features configuration.
+    """
+
+    # Filter features related to this dataset
+    dataset_df = features_config[features_config["name"]==dataset_name]
+    if dataset_df.empty:
+        raise ValueError(f"Error: No features for {dataset_name}")
+    if field_type == 'feature':
+        feature_field_types = ['past_covariate', 'future_covariate', 'static_covariate']
+    else:
+        feature_field_types = [field_type]
+    filtered_features = dataset_df[dataset_df["field_type"].isin(feature_field_types)]
+    return filtered_features
+
+
+def create_id_section(
+        dataset_name: str,
+        features_config: pd.DataFrame) -> Dict:
+    """
+    Create the id section of the schema.
+
+    Args:
+    dataset_name (str): The name of the dataset.
+    features_config (pd.DataFrame): The features configuration data.
+
+    Returns:
+    Dict: The id section of the schema.
+    """
+    # Filter features related to this dataset
+    filtered = filter_features_for_dataset(
+        dataset_name, "id", features_config
+    )
+    if filtered.empty or filtered.shape[0] > 1:
+        raise ValueError(f"Error: No id field or more than id field for {dataset_name}")
+    # Create the id section
+    field_section = {
+        "name": filtered["field_name"].values[0],
+        "description": filtered["field_description"].values[0],
+    }
+    return field_section
+
+
+def create_target_section(
+        dataset_name: str,
+        dataset: pd.DataFrame,
+        features_config: pd.DataFrame) -> Dict:
+    """
+    Create the target section of the schema.
+
+    Args:
+    dataset_name (str): The name of the dataset.
+    dataset (pd.DataFrame): The dataset.
+    features_config (pd.DataFrame): The features configuration data.
+
+    Returns:
+    Dict: The target section of the schema.
+    """
+
+    filtered = filter_features_for_dataset(
+        dataset_name, "target", features_config
+    )
+    if filtered.empty or filtered.shape[0] > 1:
+        raise ValueError(f"Error: No target field or more than target field for {dataset_name}")
+
+    # Create the target section
+    target_name = filtered["field_name"].values[0]
+    data_type = filtered["data_type"].values[0]
+    field_section = {
+        "name": target_name,
+        "description": filtered["field_description"].values[0],
+        "dataType": data_type,
+        "example": dataset[target_name].dropna().iloc[0]
+    }    
+    return field_section
+
+
+def create_time_section(
+        dataset_name: str,
+        dataset: pd.DataFrame,
+        features_config: pd.DataFrame) -> Union[None, Dict]:
+    """
+    Create the time section of the schema.
+
+    Args:
+    dataset_name (str): The name of the dataset.
+    dataset (pd.DataFrame): The dataset.
+    features_config (pd.DataFrame): The features configuration data.
+
+    Returns:
+    Union[None, Dict]: The time section of the schema.
+    """
+    
+    filtered = filter_features_for_dataset(
+        dataset_name, "time", features_config
+    )
+    # If no time field in the dataset, return None
+    if filtered.empty:
+        return None
+
+    if filtered.shape[0] > 1:
+        raise ValueError(f"Error: More than one time field for {dataset_name}")
+
+    # Create the target section
+    time_field_name = filtered["field_name"].values[0]
+    data_type = filtered["data_type"].values[0]
+    field_section = {
+        "name": time_field_name,
+        "description": filtered["field_description"].values[0],
+        "dataType": data_type,
+        "example": dataset[time_field_name].dropna().iloc[0]
+    }
+    return field_section
+
+
+def create_feature_section(
+        dataset_name: str,
+        dataset_row: pd.Series,
+        dataset: pd.DataFrame,
+        features_config: pd.DataFrame) -> List[Dict]:
+    """
+    Create the feature section of the schema.
+
+    Args:
+    dataset_name (str): The name of the dataset.
+    dataset_row (pd.Series): The metadata for the dataset.
+    dataset (pd.DataFrame): The dataset.
+    features_config (pd.DataFrame): The features configuration data.
+
+    Returns:
+    List[Dict]: The features section of the schema.
+    """
+    features_config = features_config[features_config['name'] == dataset_row['name']]
+    # Filter features related to this dataset
+    filtered = filter_features_for_dataset(
+        dataset_name, "feature", features_config
+    )
+    # create the features section
+    past_covariates = []
+    future_covariates = []
+    static_covariates = []
+    if filtered.empty:
+        # no exogenous covariates
+        return past_covariates, future_covariates, static_covariates
+    for _, feature_row in filtered.iterrows():
+        feature = {
+            "name": feature_row['field_name'],
+            "description": feature_row['field_description'],
+            "dataType": feature_row['data_type'].upper(),
+            "example": dataset[feature_row['field_name']].dropna().iloc[0]
+        }
+        if feature_row['field_type'] == 'past_covariate':
+            past_covariates.append(feature)
+        elif feature_row['field_type'] == 'future_covariate':
+            future_covariates.append(feature)
+        elif feature_row['field_type'] == 'static_covariate':
+            static_covariates.append(feature)
+        else:
+            raise ValueError(f"Error: Unknown feature type for {dataset_name}")
+    return past_covariates, future_covariates, static_covariates
+
+
+def generate_schemas(
+        dataset_metadata: pd.DataFrame,
+        processed_datasets_path: str,
+        features_config: pd.DataFrame):
+    """
+    Generate the schema for each dataset.
+
+    Args:
+    dataset_metadata (pd.DataFrame): The metadata for all the datasets.
+    processed_datasets_path (str): The path where the processed datasets are stored.
+    features_config (pd.DataFrame): The features configuration data.
+    """
+    schemas=[]
+    dataset_names=[]
+
+    # Iterate through all datasets marked for use in the metadata
+    for _, dataset_row in \
+        dataset_metadata[dataset_metadata['use_dataset'] == 1].iterrows():
+
+        if dataset_row["use_dataset"] == 0:
+            continue
+
+        dataset_name = dataset_row["name"].strip()
+        print("Creating schema for dataset", dataset_name)
+        schema = {}
+        schema["title"] = dataset_row["title"]
+        schema["description"] = dataset_row["description"]
+        schema["modelCategory"] = dataset_row["model_category"]
+        schema["schemaVersion"] = 1.0
+        schema["inputDataFormat"] = "CSV"
+        schema["encoding"] = dataset_row["encoding"]
+        schema["frequency"] = dataset_row["frequency"]
+        schema["forecastLength"] = dataset_row["forecast_length"]
+
+        # read dataset
+        dataset = pd.read_csv(os.path.join(
+            processed_datasets_path, dataset_name, f"{dataset_name}.csv"))
+
+        schema["idField"] = create_id_section(dataset_name, features_config)
+
+        time_section = create_time_section(
+            dataset_name, dataset, features_config)
+
+        if time_section is not None:
+            schema["timeField"] = time_section
+
+        schema["forecastTarget"] = create_target_section(
+            dataset_name, dataset, features_config)
+
+        past_covariates, future_covariates, static_covariates = \
+            create_feature_section(
+                dataset_name, dataset_row, dataset, features_config)
+
+        schema["pastCovariates"] = past_covariates
+        schema["futureCovariates"] = future_covariates
+        schema["staticCovariates"] = static_covariates
+
+        schemas.append(schema)
+        dataset_names.append(dataset_name)
+
+    # Write the schemas in JSON format to disk
+    for dataset_name, schema in zip(dataset_names, schemas):
+        output_fpath = os.path.join(
+            processed_datasets_path,
+            dataset_name,
+            f"{dataset_name}_schema.json"
+        )
+        with open(output_fpath, "w", encoding="utf-8") as file_:
+            json.dump(schema, file_, cls=JSONEncoder, indent=2)
+
+
+def run_schema_gen():
+    """Generate the schema for each dataset."""
+    dataset_metadata = load_metadata(dataset_cfg_path=paths.dataset_cfg_path)
+    features_config = load_features_config(
+        features_cfg_path=paths.features_cfg_path)\
+            .apply(strip_quotes)
+
+    generate_schemas(
+        dataset_metadata=dataset_metadata,
+        processed_datasets_path=paths.processed_datasets_path,
+        features_config=features_config
+    )
+
+
+if __name__ == "__main__":
+    run_schema_gen()
